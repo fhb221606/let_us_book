4e77bc25a5bae15996e4ff358d7ed06ba65804af
fhb221598 <162293449+fhb221598@users.noreply.github.com>
2024-05-10 13:28:40 +0200

    +added master data table

--- let_us_book/src/main/java/let_us_book/start_window.java
+++ let_us_book/src/main/java/let_us_book/start_window.java
@@ -8,11 +8,17 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import java.awt.Font;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import java.awt.Component;
+import java.awt.Color;
+import javax.swing.border.LineBorder;
 
 public class start_window extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTable master_table;
 
 	/**
 	 * Launch the application.
@@ -37,16 +43,54 @@ public class start_window extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 964, 613);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 255, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Hello World");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblNewLabel.setBounds(25, 27, 167, 51);
+		JLabel masterLabel = new JLabel("Hello World");
+		masterLabel.setForeground(new Color(0, 0, 0));
+		masterLabel.setBackground(new Color(213, 0, 0));
+		masterLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
+		masterLabel.setBounds(25, 28, 136, 40);
 		
-		lblNewLabel.setText("Hello World 2");
-		contentPane.add(lblNewLabel);
+		masterLabel.setText("Master Data");
+		contentPane.add(masterLabel);
+		
+		master_table = new JTable();
+		master_table.setBorder(new LineBorder(new Color(0, 0, 0)));
+		master_table.setRowHeight(30);
+		master_table.setRowSelectionAllowed(false);
+		master_table.setFillsViewportHeight(true);
+		master_table.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		master_table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{"Category", "Establishments", "Rooms", "Beds"},
+				{"*****", null, null, null},
+				{"****", null, null, null},
+				{"***", null, null, null},
+				{"** & *", null, null, null},
+				{"Total", null, null, null},
+			},
+			new String[] {
+				"Category", "Establishments", "Rooms", "Beds"
+			}
+		) {
+			Class[] columnTypes = new Class[] {
+				String.class, String.class, String.class, String.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+		});
+		master_table.getColumnModel().getColumn(0).setPreferredWidth(100);
+		master_table.getColumnModel().getColumn(1).setPreferredWidth(100);
+		master_table.getColumnModel().getColumn(2).setPreferredWidth(100);
+		master_table.getColumnModel().getColumn(3).setPreferredWidth(100);
+		master_table.setBounds(25, 89, 887, 180);
+		
+		
+		contentPane.add(master_table);
 	}
 }
